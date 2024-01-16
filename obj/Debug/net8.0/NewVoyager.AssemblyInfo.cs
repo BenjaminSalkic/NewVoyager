376f8f30ad87da6af51e1bfda6ce403140e3e240
smakivic <smakivic03@gmail.com>
2024-01-16 18:57:13 +0100

    fix for azure(nadam se)

--- obj/Debug/net8.0/NewVoyager.AssemblyInfo.cs
+++ obj/Debug/net8.0/NewVoyager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewVoyager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3903061c84a0d2b4281fe0139a3cd92061376b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2140c3189a25dbf75e3efae3e8fbf660004c53d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewVoyager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewVoyager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
